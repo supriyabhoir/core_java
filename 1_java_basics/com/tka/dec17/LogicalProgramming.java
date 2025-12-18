8dbd26ef0f9c30b98d27a8d9428f4c26f388f583
rgbhoir <sup23bh@gmail.com>
2025-12-18 22:57:52 +0530

    first commit

---
+++ 1_java_basics/com/tka/dec17/LogicalProgramming.java
@@ -0,0 +1,27 @@
+package com.tka.dec17;
+
+public class LogicalProgramming {
+	void addMethod(int n1,int n2) {
+		System.out.println("Addition:"+(n1+n2));
+		}
+	void subMethod(int n1,int n2) {
+		System.out.println("Subtraction:"+(n1-n2));
+		}
+	void divMethod(int n1,int n2) {
+		System.out.println("Division:"+(n1/n2));
+		}
+	void moduloMethod(int n1,int n2) {
+		System.out.println("Modulo:"+(n1%n2));
+		}
+	void GreatnumMethod(int n1,int n2) {
+		System.out.println("Greater:"+(n1>n2));
+		}
+	void LessnumMethod(int n1,int n2) {
+		System.out.println("Lessthan:"+(n1<n2));
+		}
+	void equalMethod(int n1,int n2) {
+		System.out.println("Equal:"+(n1==n2));
+		}
+	
+	
+}
---
+++ 1_java_basics/com/tka/dec17/Test.java
@@ -0,0 +1,27 @@
+package com.tka.dec17;
+
+public class Test {
+public static void main(String[] args) {
+	int n1=88;
+	int n2=77;
+	n1+=4;
+	n2-=5;
+	
+	
+	
+	LogicalProgramming obj=new LogicalProgramming();
+	obj.addMethod(77, 66);
+	obj.divMethod(66, 5);
+	obj.equalMethod(10, 10);
+	obj.subMethod(10, 8);
+	obj.equalMethod(110, 011);
+	obj.GreatnumMethod(10, 40);
+	obj.LessnumMethod(40, 30);
+	
+	System.out.println("After+=:"+n1);
+
+	System.out.println("After 2");
+
+	
+}
+}
